d1b76500fe4b5abadf29436a95ddb0cd67e81b3e
SadkiratSingh <sadkiratsingh@yahoo.com>
2021-09-06 18:34:19 +0530

    search functionality completed

--- src/Components/users/Search.js
+++ src/Components/users/Search.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
 class Search extends Component{
+    constructor(){
+        super();
+        this.state = {
+            text:''
+        }
+    }
+    onChangeHandler = (event) =>{
+        this.setState({text:event.target.value});
+    }
+    onSubmitHandler(event){
+        event.preventDefault();
+        this.props.onSearchUsers(this.state.text);
+        this.setState({text:''});
+    }
     render(){
         return(
-            <form className='form'>
-                <input type='text' name='text' placeholder = 'search users ...' />
+            <form onSubmit = {this.onSubmitHandler.bind(this)} className = 'form'>
+                <input type='text' name='text' placeholder = 'search users ...' value = {this.state.text} onChange = {this.onChangeHandler} />
                 <input type='submit' value='search' className = 'btn btn-dark btn-block' />
             </form>
         )
