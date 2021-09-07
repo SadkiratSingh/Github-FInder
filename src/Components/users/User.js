ff1bd1c955bf7cc315dd1197eb1433e13effc358
SadkiratSingh <sadkiratsingh@yahoo.com>
2021-09-07 17:41:30 +0530

    displayed github user info

--- src/Components/users/User.js
+++ src/Components/users/User.js
@@ -1,8 +1,104 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Spinner from "../Layouts/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 class User extends Component {
+  componentDidMount() {
+    this.props.onGetUser(this.props.match.params.login);
+  }
+
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    onGetUser: PropTypes.func.isRequired,
+  };
+
   render() {
-    return <div>User</div>;
+    const {
+      name,
+      avatar_url,
+      location,
+      bio,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      hireable,
+      company,
+    } = this.props.user;
+
+    const { loading } = this.props;
+    if (loading) {
+      return <Spinner />;
+    }
+    return (
+      <Fragment>
+        {/* Link basically renders the anchor tag only */}
+        <Link to="/" className="btn btn-light">
+          Back To Search
+        </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className="fas fa-check text-success"></i>
+        ) : (
+          <i className="fas fa-times-circle text-danger"></i>
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              alt=""
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>{location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username:</strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company:</strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website:</strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers:{followers}</div>
+          <div className="badge badge-success">Following:{following}</div>
+          <div className="badge badge-danger">Public Repos:{public_repos}</div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
