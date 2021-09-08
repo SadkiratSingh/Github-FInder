6e226673131392ba44194134cfa3d788e79e1d4d
SadkiratSingh <sadkiratsingh@yahoo.com>
2021-09-09 00:02:36 +0530

    App complete ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./Components/Layouts/Navbar";
-import Users from "./Components/users/Users";
-import Search from "./Components/users/Search";
+import Home from "./Components/pages/Home";
+import NoComponent from "./Components/pages/NoComponent";
 import Alert from "./Components/Layouts/Alert";
 import About from "./Components/pages/About";
 import User from "./Components/users/User";
@@ -22,24 +22,16 @@ const App = () => {
               {/* put all routes in a switch */}
               <Switch>
                 {/* index route */}
-                <Route
-                  exact
-                  path="/"
-                  render={() => {
-                    return (
-                      <Fragment>
-                        <Search />
-                        <Users />
-                      </Fragment>
-                    );
-                  }}
-                />
+                <Route exact path="/" component={Home} />
 
                 {/* about route */}
                 <Route exact path="/about" component={About} />
 
                 {/* user view route */}
                 <Route exact path="/user/:login" component={User} />
+
+                {/* any other route */}
+                <Route component={NoComponent} />
               </Switch>
             </div>
           </div>
---
+++ src/Components/pages/NoComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoComponent = () => {
+  return (
+    <div>
+      <h3 style={{ textAlign: "center" }}>No Content here</h3>
+    </div>
+  );
+};
+
+export default NoComponent;
